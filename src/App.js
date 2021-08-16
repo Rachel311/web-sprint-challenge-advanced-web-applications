a62c3e20cf545c074ffb68d4cc443e9311049e37
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-08-15 19:38:26 -0600

    completed mvp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import {PrivateRoute} from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           Color Picker Sprint Challenge
           <a data-testid="logoutButton" href="#">logout</a>
         </header>
+        <Route exact path="/" component={Login} />
+        <PrivateRoute path='/bubbles' component={BubblePage} />
       </div>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { axiosWithAuth } from '../helpers/axiosWithAuth';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [form, setForm] = useState({username: '', password: ''})
+  const { push } = useHistory();
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth().post('/login', form)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        push('/bubbles') 
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {})
 
   const error = "";
   //replace with error state
@@ -11,7 +35,24 @@ const Login = () => {
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <h2>Login Here</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='username'>Username: </label>
+          <input
+            id='username'
+            name='username'
+            type='text'
+            onChange={handleChange}
+            />
+          <label htmlFor='password'>Password: </label>
+          <inuput
+            id='password'
+            name='password'
+            type='password'
+            onChange={handleChange}
+            />
+          <button id='submit' type='submit'>Login</button>  
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
